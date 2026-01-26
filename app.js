dc57149199c8a0cc91d83d39c178fe1c725bd85a
Satish Raut <satshraut@gmail.com>
2026-01-26 12:32:43 +0530

    Refactor URL shortener to Express.js with middleware and routing

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ import { readFile, writeFile } from "fs/promises";
 import { createServer } from "http";
 import path from "path";
 import crypto from "crypto";
+import express from "express";
 
 const PORT = 3000;
 const DATA_FILE = path.join("data", "links.json");
 
-const serveFile = async (res, filePath, contentType) => {
-  try {
-    const data = await readFile(filePath);
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(data);
-  } catch (error) {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Page not found");
-  }
-};
+// const serveFile = async (res, filePath, contentType) => {
+//   try {
+//     const data = await readFile(filePath);
+//     res.writeHead(200, { "Content-Type": contentType });
+//     res.end(data);
+//   } catch (error) {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("404 Page not found");
+//   }
+// };
 
 const loadLinks = async () => {
   try {
@@ -36,80 +37,174 @@ const saveLinks = async (links) => {
   await writeFile(DATA_FILE, JSON.stringify(links, null, 2));
 };
 
-const server = createServer(async (req, res) => {
-  console.log("Request Error: ", req.url);
+// "<------------------- ExpressJS -------------------->"
 
-  // GET method
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      return serveFile(res, path.join("public", "index.html"), "text/html");
-    } else if (req.url === "/style.css") {
-      return serveFile(res, path.join("public", "style.css"), "text/css");
-    } else if (req.url === "/bgimg.jpg") {
-      return serveFile(res, path.join("public", "bgimg.jpg"), "image/jpeg");
-    } else if (req.url === "/links") {
-      const links = await loadLinks(); // It returns all the links in js object format
+// Create server usign ExpressJs
+const app = express();
 
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify(links));
-    } else {
-      // Links Redirection logic
+app.use(express.static("public")); // Public folder contains only the css and the background image file as static file
+app.use(express.urlencoded({ extended: true }));
 
-      const links = await loadLinks();
-      const shortCode = req.url.slice(1);
-      console.log("Incoming request:", req.method, req.url);
+/* NOTE:  
+  `express.urlencoded({ extended: true }) is middleware that parses URL-encoded form data sent by the browser and converts it into a JavaScript object available on req.body, with support for nested objects when extended is set to true.`
+*/
 
-      if (links[shortCode]) {
-        res.writeHead(302, { location: links[shortCode] });
-        return res.end();
-      }
+// Express GET Method
+app.get("/", async (req, res) => {
+  try {
+    const file = await readFile(path.join("views", "index.html"));
+    const links = await loadLinks(); // It returns all the links in js object format
 
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("404 Page not found");
-    }
-  }
+    // console.log("Loaded Links:\n", links, typeof links);
 
-  // POST method
-  if (req.method === "POST" && req.url == "/shorten") {
-    const links = await loadLinks(); // Load the existing data first
+    // Update the front-end
+    const content = file.toString().replaceAll(
+      "{{shortened-urls}}",
+      Object.entries(links)
+        .map(([shortCode, url]) => {
+          return `
+                  <a href="/${shortCode}" target="_blank"> 
+                  ${req.host}/${shortCode}</a> - ${url}
+              `;
+        })
+        .join(""),
+    );
 
-    let body = "";
-    req.on("data", (chunk) => (body += chunk));
-
-    req.on("end", async () => {
-      console.log(body);
-
-      // Recived String from the frontend converted to JS Object
-      const { url, shortCode } = JSON.parse(body); // From the form we get 2 data that the url and shortform of that url
-
-      if (!url) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        return res.end("URL is Rquired!");
-      }
-
-      //NOTE: If the short code is not provided by the user then create a shortcode first
-      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-
-      //NOTE: Check the dulicate shortcode in the Data file
-      if (links[finalShortCode]) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        return res.end(
-          "This shortcode is already exist. Please Choose another shortcode!",
-        );
-      }
-
-      //NOTE: If everything is fine then add it to the data file
-      links[finalShortCode] = url;
-      await saveLinks(links);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(
-        JSON.stringify({ success: true, shortcode: finalShortCode }),
-      );
-    });
+    return res.send(content);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error!");
   }
 });
 
-server.listen(PORT, () => {
+// URL Redirect Logic
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    console.log(req.params)
+    const links = await loadLinks();
+
+    if (!links[shortCode]) return res.status(404).send("404 Error Occured!");
+
+    return res.redirect(links[shortCode]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error!");
+  }
+});
+
+// Express POST Method
+app.post("/", async (req, res) => {
+  try {
+    const links = await loadLinks(); // Load the existing data first
+    const { url, shortCode } = req.body;
+
+    if (!url) {
+      return res.send("URL is Rquired!");
+    }
+
+    //NOTE: If the short code is not provided by the user then create a shortcode first
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    //NOTE: Check the dulicate shortcode in the Data file
+    if (links[finalShortCode]) {
+      return res
+        .status(400)
+        .send(
+          "This shortcode is already exist. Please Choose another shortcode!",
+        );
+    }
+
+    //NOTE: If everything is fine then add it to the data file
+    links[finalShortCode] = url;
+    await saveLinks(links);
+
+    return res.redirect("/")
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error!");
+  }
+});
+
+// "<--------------------- NodeJs --------------------->"
+
+// Create the server in NodeJs
+// const server = createServer(async (req, res) => {
+//   console.log("Request url: ", req.url);
+
+//   // GET method
+//   if (req.method === "GET") {
+//     if (req.url === "/") {
+//       return serveFile(res, path.join("public", "index.html"), "text/html");
+//     } else if (req.url === "/style.css") {
+//       return serveFile(res, path.join("public", "style.css"), "text/css");
+//     } else if (req.url === "/bgimg.jpg") {
+//       return serveFile(res, path.join("public", "bgimg.jpg"), "image/jpeg");
+//     } else if (req.url === "/links") {
+//       const links = await loadLinks(); // It returns all the links in js object format
+
+//       res.writeHead(200, { "Content-Type": "application/json" });
+//       return res.end(JSON.stringify(links)); // JS object to JSON String
+//     } else {
+//       // Links Redirection logic
+//       // `Form submission is by default GET method so, when the form is submitted the data will come over here`
+
+//       const links = await loadLinks();
+//       const shortCode = req.url.slice(1);
+//       console.log("Incoming request:", req.method, req.url);
+
+//       if (links[shortCode]) {
+//         res.writeHead(302, { location: links[shortCode] });
+//         return res.end();
+//       }
+
+//       res.writeHead(404, { "Content-Type": "text/plain" });
+//       res.end("404 Page not found");
+//     }
+//   }
+
+//   // POST method
+//   if (req.method === "POST" && req.url == "/shorten") {
+//     const links = await loadLinks(); // Load the existing data first
+
+//     let body = "";
+//     req.on("data", (chunk) => (body += chunk));
+
+//     req.on("end", async () => {
+//       console.log(body);
+
+//       // Recived String from the frontend converted to JS Object
+//       const { url, shortCode } = JSON.parse(body); // From the form we get 2 data that the url and shortform of that url
+
+//       if (!url) {
+//         res.writeHead(400, { "Content-Type": "text/plain" });
+//         return res.end("URL is Rquired!");
+//       }
+
+//       //NOTE: If the short code is not provided by the user then create a shortcode first
+//       const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+//       //NOTE: Check the dulicate shortcode in the Data file
+//       if (links[finalShortCode]) {
+//         res.writeHead(400, { "Content-Type": "text/plain" });
+//         return res.end(
+//           "This shortcode is already exist. Please Choose another shortcode!",
+//         );
+//       }
+
+//       //NOTE: If everything is fine then add it to the data file
+//       links[finalShortCode] = url;
+//       await saveLinks(links);
+
+//       res.writeHead(200, { "Content-Type": "application/json" });
+//       return res.end(
+//         JSON.stringify({ success: true, shortcode: finalShortCode }),
+//       );
+//     });
+//   }
+// });
+
+app.listen(PORT, () => {
   console.log(`Server Running at http://localhost:${PORT}`);
 });
